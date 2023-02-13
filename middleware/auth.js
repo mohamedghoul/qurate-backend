8237ad45bd484fefbabc890d654872649ce658a4
Mohamed Ghoul <ghoul_mohamed@outlook.com>
2023-02-13 18:15:33 +0300

    Modify token verification function
    - Modified the token verification function to give more detailed error messages for different errors

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,11 +5,11 @@ export const verifyToken = (req, res, next) => {
     try {
         const token = req.headers.authorization;
         if (!token) {
-            return res.status(401).json({ error: 'Access denied' });
+            return res.status(401).json({ error: 'Access denied: Token not provided' });
         }
         const verified = jwt.verify(token, process.env.JWT_SECRET);
         if (!verified) {
-            return res.status(401).json({ error: 'Access denied' });
+            return res.status(401).json({ error: 'Access denied: Invalid token' });
         }
         req.user = verified;
         next();
